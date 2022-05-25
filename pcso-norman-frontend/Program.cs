e2285f1b98d494f5c3ebee9a5d56f43704ba628e
nsespiritu@yahoo.com <nsespiritu@yahoo.com>
2022-05-25 10:49:31 +0800

    include mudblazer

--- pcso-norman-frontend/Program.cs
+++ pcso-norman-frontend/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using pcso_norman_frontend;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// new code
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
